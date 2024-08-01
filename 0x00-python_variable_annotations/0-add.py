08e5bc1fa59d8ce77e9ea478adf00ad1234ca40e
Azeez Adebayo <debaycisse@gmail.com>
2024-08-01 03:59:36 -0700

    add the README and implement task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module houses the definition for a add() function.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Sums up two give float type number.
+
+    Args:
+        a - variable for the first number
+        b - variable for the second number
+
+    Returns:
+        the sum value of the two numbers
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,8 @@
+# Python Variable Annotation
+
+This directory collects a set of tasks that are related to
+the concept of variable annotation in python.
+
+It is a concept with which a python developer ensures that
+datatypes are correctly given for variables and other components
+of a python program.
