7420cd337ce83bff15f9cc94101ce143ec2d0fb9
Azeez Adebayo <debaycisse@gmail.com>
2024-08-01 04:54:03 -0700

    implement task 4
    4. Define variables
    add annotated integer, float, boolean, and string variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains an annotated version of
+an integer, a float, a boolean, and a string variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
