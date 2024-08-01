5e2e6f6363703bb09f8b3ead4113a3a9c383bd95
Azeez Adebayo <debaycisse@gmail.com>
2024-08-01 04:43:19 -0700

    implement task 3
    3. Basic annotations - to string
    add an annotated function that converts a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This modules houses the definition of a function, named to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a given float data to a string equivalent
+
+    Args:
+        n - variable that holds the float value to be converted
+
+    Returns:
+        the string equivalent of the converted float data
+    """
+    return "{}".format(n)
