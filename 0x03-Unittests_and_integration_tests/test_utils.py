992c72e871fef89224292b36e68fd8b742c657e4
Azeez Adebayo <debaycisse@gmail.com>
2024-08-24 19:36:33 +0000

    implement task 1
    tests that an assert is raised for non-existing key(s)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Tests access_nested_map function, with each of the parameters"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"]),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Tests that excecption is raised for non-existing key(s)"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
