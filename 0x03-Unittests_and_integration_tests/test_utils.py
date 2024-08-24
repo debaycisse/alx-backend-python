786a344c5ccc7a6d4bf3b150b9dd76211934cd69
Azeez Adebayo <debaycisse@gmail.com>
2024-08-24 22:04:36 +0000

    implement task 2
    2. Mock HTTP calls
    tests a function that mocks a request's get method using the mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """This module houses definition of the tests for the util.py module"""
-import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+
+import unittest
+import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,13 +18,31 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Tests access_nested_map function, with each of the parameters"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ["a"]),
         ({"a": 1}, ["a", "b"]),
-    ])
+        ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Tests that excecption is raised for non-existing key(s)"""
         with self.assertRaises(KeyError):
-            access_nested_map(nested_map, path)
+            utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests get_json function with various inputs"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Tests the get_json function, using mock object"""
+        with patch('utils.requests.get') as mocked_get:
+            mocked_get.return_value.json.return_value = test_payload
+
+            utils.get_json(test_url)
+            mocked_get.assert_called_once_with(test_url)
+
+            self.assertEqual(utils.get_json(test_url), test_payload)
